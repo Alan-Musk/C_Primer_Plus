733f1c946dada3fd9df6149f4e91267fe8faa404
AlanMusk <59758888+Alan-Musk@users.noreply.github.com>
2024-01-26 10:47:57 +0800

    Code fix

--- Chapter_09/编程练习/test_02.c
+++ Chapter_09/编程练习/test_02.c
@@ -1,30 +1,29 @@
 // 设计一个函数chline(ch,i,j),打印指定的字符j行i列.在一个简单的驱动程序中测试该函数
 #include <stdio.h>
 
-void chline(char ch,int i,int j);
+void chline(char ch, int i, int j);
 
-int main(void)
-{
+int main(void) {
     char ch;
-    int rows,cols;
-    printf("请输入要打印的字符:");
-    scanf("%c",&ch);
-    printf("请输入要打印的行和列");
-    scanf("%d %d",&rows,&cols);
-    chline(ch,rows,cols);
+    int i, j;
+
+    printf("请输入要打印的字符: ");
+    scanf("%c", &ch);
+    printf("请输入开始和结束的列数（例如 3 7）: ");
+    scanf("%d %d", &i, &j);
+
+    chline(ch, i, j);
 
     return 0;
 }
 
-void chline(char ch,int i,int j)
-{
-    int row,cols;
-    for(row=0;row<i;row++)
-    {
-        for(cols=0;cols<j;cols++)
-        {
-            printf("%c",ch);
-        }
-        printf("\n");
+void chline(char ch, int i, int j) {
+    for (int col = 1; col < i; col++) {
+        printf(" "); // 在打印字符之前打印空格，直到达到开始列
     }
+    for (int col = i; col <= j; col++) {
+        printf("%c", ch); // 从开始列到结束列打印字符
+    }
+    printf("\n");
 }
+
